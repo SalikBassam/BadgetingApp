0be56a6dbabe5dd9fdb978e266068c764f979c75
Salik Bassam <117837220+SalikBassam@users.noreply.github.com>
2023-05-11 11:03:50 +0100

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,16 @@
+import {Header,Footer,Container} from './Layouts/index'
+
+
+function App() {
+
+  return ( 
+    
+    <>
+     <Header />
+      <p>Test</p>
+     <Footer />
+    </>
+  );
+}
+
+export default App;
