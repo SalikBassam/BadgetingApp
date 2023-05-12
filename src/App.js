d9f5cc1804a2d569afa46e00c2706b5202d42d7e
Salik Bassam <117837220+SalikBassam@users.noreply.github.com>
2023-05-12 05:53:55 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {Header,Footer} from './Layouts/index'
+import {StatisticsPage,TransactionForm,TransactionList} from './Components/index'
+
+
+function App() {
+
+  return ( 
+    
+    <>
+     <Header />
+     <StatisticsPage />
+     <TransactionForm />
+     {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Transaction Form/TransactionForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../Assets/App.css'
+import './TransactionForm.css'
+
+function TransactionForm() {
+  return (
+    <section>
+<div className='container'>
+<div className='title'>
+      <h2>Add New <span> Transaction</span></h2>
+      <hr />
+      </div>
+</div>
+    </section>
+  )
+}
+
+export default TransactionForm
