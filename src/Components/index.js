fa70cd9a5b826563b576c8959020bd64bd714473
Salik Bassam <117837220+SalikBassam@users.noreply.github.com>
2023-05-16 12:08:37 +0200

    Delete src directory

--- src/Components/index.js
+++
@@ -1,3 +0,0 @@
-export {default as StatisticsPage} from './Statistics Page/StatisticsPage'
-export {default as TransactionForm} from './Transaction Form/TransactionForm'
-export {default as TransactionList} from './Transaction List/TransactionList'
